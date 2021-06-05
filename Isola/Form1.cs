95c92310ce2a08cd0f1641dd1a697361194d2781
VencislavKA <57410823+VencislavKA@users.noreply.github.com>
2021-06-05 11:20:56 +0300

    Start screen

--- Isola/Form1.cs
+++ Isola/Form1.cs
@@ -14,7 +14,85 @@ namespace Isola
     {
         public Form1()
         {
+            WindowState = FormWindowState.Maximized;
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int parsedSize;
+            int parsedName;
+            if (string.IsNullOrEmpty(name1.Text))
+            {
+                MessageBox.Show("Невалидно име за играч едно");
+            }
+            else if (string.IsNullOrEmpty(name2.Text))
+            {
+                MessageBox.Show("Невалидно име за играч две");
+            }
+            else if (!int.TryParse(size.Text, out parsedSize))
+            {
+                MessageBox.Show("Игралното поле не може да е текст или празно");
+            }
+            else if (name1.Text == name2.Text)
+            {
+                MessageBox.Show("Имената на играчите съвпадат");
+            }
+            else if (name1.Text.Length > 10)
+            {
+                MessageBox.Show("Името на играч едно е прекалено дълго");
+            }
+            else if (name2.Text.Length > 10)
+            {
+                MessageBox.Show("Името на играч две е прекалено дълго");
+            }
+            else if (parsedSize > 50 || parsedSize <= 1 || parsedSize % 2 == 0)
+            {
+                MessageBox.Show("Невалидно игрално поле");
+            }
+            else if (int.TryParse(name1.Text,out parsedName) && int.TryParse(name2.Text, out parsedName))
+            {
+                MessageBox.Show("Името не може да е само цифри");
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
