079e5e904da8ddf5bd92115376bca6f5cd6e7a49
VencislavKA <57410823+VencislavKA@users.noreply.github.com>
2021-06-05 12:23:49 +0300

    names

--- Isola/Form1.Designer.cs
+++ Isola/Form1.Designer.cs
@@ -175,7 +175,7 @@ namespace Isola
             this.Controls.Add(this.Title);
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Начало";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
