95c33f2bea71cbd1e27c8d91707e7d464604a8eb
Competition <Competition@LAPTOP-3QB175CT>
2021-06-05 08:00:38 +0100

    Game logic work

--- Game_logic/GameController.cs
+++ Game_logic/GameController.cs
@@ -12,28 +12,45 @@ namespace Game_logic
         public Player Player_One { get; private set; }
         public Player Player_Two { get; private set; }
 
+        public bool Game_ended = false;
         public enum Turn_State
         {
             Move=1,
             Block=2
         }
 
+        public Turn_State Current_State { get; private set; }
+
+        public Player Current_Player { get; private set; }
+
         public GameController(Player one,Player two,Game_board board)
         {
             Player_One = one;
             Player_Two = two;
+
+            Current_Player = Player_One;
+
             Board = board;
         }
 
         public void BlocTile(int x,int y)
         {
-            Board[x , y] = Game_board.Tile_State.Blocked;
+            if (Current_State != Turn_State.Block)
+                throw new Exception("NOT BLOCK STATE");
+            ChangeState();
+            ChangePlayer();
+            
         }
 
-        public void Player_Move(Player p, int x, int y)
+        public void Player_Move(int x, int y)
         {
+            if(Current_State!=Turn_State.Move)
+                throw new Exception("NOT MOVE STATE");
+
             if (x > 50 || x < 0 || y > 50 || y < 0)
                 throw new ArgumentOutOfRangeException();
+
+            Player p = Current_Player;
 
             Board[p.X, p.Y] = Game_board.Tile_State.Free;
 
@@ -42,7 +59,52 @@ namespace Game_logic
 
             Board[p.X, p.Y] = Game_board.Tile_State.Player;
 
+            ChangeState();
         }
 
+        public event EventHandler Current_Player_Changed;
+        public event EventHandler Current_State_Changed;
+
+        public event EventHandler Current_State_Lose;
+
+
+        private void ChangeState()
+        {
+            Current_State = Current_State == Turn_State.Block ? Turn_State.Move : Turn_State.Block;
+            Current_State_Changed?.Invoke(this, new EventArgs());
+        }
+
+        private void ChangePlayer()
+        {
+            Current_Player = Current_Player == Player_One ? Player_Two : Player_One;
+
+          if(Check_Lose())
+          {
+                Current_State_Lose?.Invoke(this, new EventArgs());
+                Game_ended = true;
+                return;
+          }
+
+            Current_Player_Changed?.Invoke(this, new EventArgs());
+
+        }
+
+        public bool Check_Lose()
+        {
+
+            for (int x_offset = -1; x_offset < 2; x_offset++)
+            {
+                for (int y_offset = -1; y_offset < 2; y_offset++)
+                {
+                  var cell= Board[Current_Player.X + x_offset, Current_Player.Y + y_offset];
+                    if (cell == Game_board.Tile_State.Free)
+                        return false;
+
+                    
+                }
+            }
+
+            return true;
+        }
     }
 }
